8023157acb7415f9fe99b77d08c63ac04b289fe4
ricardo01010 <ricardo.2000@outlook.pt>
2021-08-02 18:04:34 +0100

    Create [id].js

---
+++ pages/produtos/[id].js
@@ -0,0 +1,10 @@
+import {useRouter} from 'next/router';
+
+function Produtos(){
+    const router = useRouter();
+    const id = router.query.id;
+
+    return <div>Id do produto {id}</div>
+}
+
+export default Produtos;
