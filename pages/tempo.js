30d2293875af6fb71ceb581c2e524007490c5531
ricardo01010 <ricardo.2000@outlook.pt>
2021-08-02 17:35:23 +0100

    Update tempo.js

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,13 +1,25 @@
-function Tempo(){
+function Tempo(props){
     const dynamicDate = new Date();
     const dynamicDateString = dynamicDate.toGMTString();
 
     return(
         <div>
-            {dynamicDateString} (dinamico)
+            <div>{dynamicDateString} (dinamico)</div>
+            <div>{props.staticDateString} (estático)</div>
         </div>
         
     )
+}
+
+export function getStaticProps(){
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
+
+    return{
+        props: {
+            staticDateString
+        }
+    }
 }
 
 export default Tempo
