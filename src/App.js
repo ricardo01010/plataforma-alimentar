653d825e85d19cdd050dccb20199f7d9d8f73dad
ricardo01010 <ricardo.2000@outlook.pt>
2021-08-05 12:21:24 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Home from './view/home';
+
+function App() {
+  return (
+    <Router>
+      <Route exact path='/' component={Home} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/view/home/index.js
@@ -0,0 +1,14 @@
+import NavebarComponent from "../../components/navebar";
+
+function App(){
+    return(
+        <>
+            <NavebarComponent/>
+            <h1>Olá</h1>
+        </>
+        
+    )
+    
+}
+
+export default App;
